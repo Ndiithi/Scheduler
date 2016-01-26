396f99347b775d7316cd4bd8e96909e0f026fdeb
mspace-developer <mspace-developer@localhost.localdomain>
2016-01-26 11:27:28 +0300

    Updated task management components from graphical wizzard

---
+++ Scheduler/build/generated-sources/ap-source-output/com/scheduler/database/DatabaseSource_.java
@@ -0,0 +1,17 @@
+package com.scheduler.database;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-26T11:22:41")
+@StaticMetamodel(DatabaseSource.class)
+public class DatabaseSource_ { 
+
+    public static volatile SingularAttribute<DatabaseSource, Integer> id;
+    public static volatile SingularAttribute<DatabaseSource, String> _password;
+    public static volatile SingularAttribute<DatabaseSource, String> _name;
+    public static volatile SingularAttribute<DatabaseSource, String> _host;
+    public static volatile SingularAttribute<DatabaseSource, String> _user;
+
+}
